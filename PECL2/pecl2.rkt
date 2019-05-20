6da9b7744685dde5d6bd82de409f3378fa04783d
Gabriel Lopez Cuenca <lopezcuenca.gabriel@gmail.com>
2019-05-20 17:51:24 +0200

    Comentarios

--- PECL2/pecl2.rkt
+++ PECL2/pecl2.rkt
@@ -6,29 +6,37 @@
 ;; Zamorano Ortega, Álvaro
 ;; ************************
 
+; Crear prisma con 3 numeros
 (define (prisma a b c) (list a b c))
 
+; Comprobar si es prisma final -> (1 1 1)
 (define (prisma_fin? x)
   (if (empty? x) #t (if (= (car x) 1) (prisma_fin? (cdr x)) #f)))
 
+; Comprobar si los valores del prisma son validos
 (define (prisma_positivo? x)
   (if (empty? x) #t (if (> (car x) 0) (prisma_positivo? (cdr x)) #f)))
 
+; Comprobar si el prisma es válido
 (define (prisma_valido? prisma)
   (if (and (not (prisma_fin? prisma)) (prisma_positivo? prisma)) #t #f))
 
+; Obtener elemento n de una lista
 (define (elemento_l lista indice)
   (list-ref lista (- indice 1)))
 
+; Generar random dado un limite
 (define (generarRandom limite) (random limite))
 
-; Funciones maximo y minimo
+; Minimo y maximo de dos numeros
 (define (min n m) (if (>= m n) n m))
 (define (max n m) (if (>= m n) m n))
 
 ;;********** CORTE VERTICAL **********
+; Generar prisma con la primera coordenada disminuida
 (define (cortar_ancho x y) (prisma (- (elemento_l x 1) y) (elemento_l x 2)(elemento_l x 3)))
 
+; Comprobar si el corte está permitido
 (define (corteAnchura prisma n)
   (if (or (>= n (elemento_l prisma 1)) (= n 0))
       (begin
@@ -36,6 +44,7 @@
         (jugarAncho prisma))
       (cortar_ancho prisma n)))
 
+; Elegir cuanto se quiere cortar
 (define (jugarAncho prisma)
   (if (> (elemento_l prisma 1) 1)
       (begin
@@ -45,16 +54,20 @@
         (display "No se puede hacer el corte")
         (pedirJugada prisma))))
 
+; Lista con todos los cortes en anchura posibles
 (define (listaCortesAncho prisma) (listaCortesAncho_aux prisma 1))
 
+; Funcion auxiliar para generar la lista de cortes posibles
 (define (listaCortesAncho_aux prisma n)
   (if (< n (elemento_l prisma 1))
       (cons (cortar_ancho prisma n) (listaCortesAncho_aux prisma (+ n 1)))
       '()))
 
 ;;********** CORTE ALTURA **********
+; Generar prisma con la segunda coordenada disminuida
 (define (cortar_alto x y) (prisma (elemento_l x 1) (- (elemento_l x 2) y) (elemento_l x 3)))
 
+; Comprobar si el corte está permitido
 (define (corteAltura prisma n)
   (if (or (>= n (elemento_l prisma 2)) (= n 0))
       (begin
@@ -62,6 +75,7 @@
         (jugarAlto prisma))
       (cortar_alto prisma n)))
 
+; Elegir cuanto se quiere cortar
 (define (jugarAlto prisma)
   (if (> (elemento_l prisma 2) 1)
       (begin
@@ -71,16 +85,20 @@
         (display "No se puede hacer el corte")
         (pedirJugada prisma))))
 
+; Lista con todos los cortes en altura posibles
 (define (listaCortesAlto prisma) (listaCortesAlto_aux prisma 1))
 
+; Funcion auxiliar para generar la lista de cortes posibles
 (define (listaCortesAlto_aux prisma n)
   (if (< n (elemento_l prisma 2))
       (cons (cortar_alto prisma n) (listaCortesAlto_aux prisma (+ n 1)))
       '()))
 
 ;;********** CORTE PROFUNDIDAD **********
+; Generar prisma con la tercera coordenada disminuida
 (define (cortar_largo x y) (prisma (elemento_l x 1) (elemento_l x 2) (- (elemento_l x 3) y)))
 
+; Comprobar si el corte está permitido
 (define (corteLongitud prisma n)
   (if (or (>= n (elemento_l prisma 3)) (= n 0))
       (begin
@@ -88,6 +106,7 @@
         (jugarLargo prisma))
       (cortar_largo prisma n)))
 
+; Elegir cuanto se quiere cortar
 (define (jugarLargo prisma)
   (if (> (elemento_l prisma 3) 1)
       (begin
@@ -97,14 +116,17 @@
         (display "No se puede hacer el corte")
         (pedirJugada prisma))))
 
+; Lista con todos los cortes en profundidad posibles
 (define (listaCortesLargo prisma) (listaCortesLargo_aux prisma 1))
 
+; Funcion auxiliar para generar la lista de cortes posibles
 (define (listaCortesLargo_aux prisma n)
   (if (< n (elemento_l prisma 3))
       (cons (cortar_largo prisma n) (listaCortesLargo_aux prisma (+ n 1)))
       '()))
 
 ;;********** CORTES POSIBLES **********
+; Generar la lista con los cortes posibles en las 3 dimensiones
 (define (listaJugadas prisma)
   (append (append (listaCortesAncho prisma) (listaCortesAlto prisma)) (listaCortesLargo prisma)))
 
@@ -143,10 +165,11 @@
       1
       (min (min 1 (nodoMinMax (car listaJugadas) 0)) (llamarMin (cdr listaJugadas)))))
 
-;Sorteamos que jugador juega primero
+;;********** FUNCIONES PARA JUGAR **********
+; Generamos aleatorio para saber quien comienza con el juego
 (define (turno) (generarRandom 2))
 
-;;********** FUNCIONES PARA JUGAR **********
+; Recibir la coordenada en la que realizar el corte
 (define (pedirJugada prisma)
   (begin
     (display "\nSeleccione [1/2/3]-[Ancho/Alto/Largo]\n");
@@ -159,6 +182,7 @@
                 (display "\nNo ha seleccionado una opcion correcta\n")
                 (pedirJugada prisma))]))))
 
+; Recibir las dimensiones del prisma inicial
 (define (pedirPrisma)
   (begin
     (display "\nIntroduce las dimensiones del prisma\n")
@@ -169,6 +193,7 @@
             (display "* Dimensiones del prisma inválidas *\n")
             (pedirPrisma))))))
 
+; Comenzar la partida con el prisma dado y el jugador inicial
 (define (jugar)
   (begin
     (let([prism (pedirPrisma)])
@@ -180,6 +205,7 @@
            (display "\n*** COMIENZA LA MAQUINA ***\n")
            (partida prism 1))))))
 
+; Control de la partida
 (define (partida prisma jugador)
   (begin
     (display "\n*** Prisma actual ")
@@ -191,11 +217,12 @@
           (partida (pedirJugada prisma) 1)
           (partida (juegaMaquina prisma) 0)))))
 
+; Dar bienvenida e iniciar juego
 (define (juego)
   (begin
     (display "\n*** BIENVENIDO AL JUEGO DE LA CANTERA ***")
     (display "\n  *** Gabriel - Sergio - Alvaro ***\n")
     (jugar)))
 
-;Ejecucion automatica del juego
+;Ejecucion del juego
 (juego)
